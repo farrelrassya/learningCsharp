33e53fe1a048fff6e251d8583251bd1fdd9d1d46
farrelrassya <91480200+farrelrassya@users.noreply.github.com>
2024-06-03 16:34:27 +0700

    Commit remaining changes

--- obj/Debug/net8.0/BasicCsharpProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/BasicCsharpProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BasicCsharpProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+055c51700a87cc645c9062505fe8ab2e974c01b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+090c90261086a251ab61015d1b229f2a97bfabd7")]
 [assembly: System.Reflection.AssemblyProductAttribute("BasicCsharpProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BasicCsharpProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
